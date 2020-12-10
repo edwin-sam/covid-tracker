7567fbe67d355cc29c40f83ec793dee8b8eac931
Edwin Sam <edwinsam@edwins-mbp.lan>
2020-12-10 00:38:21 -0800

    api data pull and materialUI install

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>hi there</h1>
-    </div>
-  );
-};
+import { Cards, Chart, CountryPicker } from './Components';
+import styles from './App.module.css';
+import { fetchData } from './api'
+
+
+class App extends React.Component {
+
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData()
+
+    this.setState({ data: fetchedData })
+  }
+
+  render() {
+    
+    const { data } = this.state; 
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data}/>
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
+}
 
 export default App;
